6f0038bf7634eee032ed18ed315427cd2c66cb1e
Sujin Lee <krispediadot@gmail.com>
2020-07-14 15:41:19 +0900

    Add: Graphs/

---
+++ Graphs/boj_1260.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<queue>
+#define SIZE 1001
+#define CONNECTED 1
+#define VISITED -1
+
+using namespace std;
+int N, M, startIndex;
+int adjArr[SIZE][SIZE]={0};
+queue<int> q;
+
+void getInput(){
+    cin>>N>>M>>startIndex;
+
+    for(int i=0; i<M; i++){
+        int node1, node2;
+        cin>>node1>>node2;
+        adjArr[node1][node2] = CONNECTED;
+        adjArr[node2][node1] = CONNECTED;
+    }
+}
+void BFS(int startIndex){
+    q.push(startIndex);
+    adjArr[startIndex][0] = VISITED;
+
+    while(!q.empty()){
+        int checkIndex = q.front();
+        q.pop();
+
+        cout<<checkIndex<<" ";
+        for(int i=1; i<=N; i++){
+            if(adjArr[checkIndex][i]==CONNECTED && adjArr[i][0]!=VISITED){
+                adjArr[i][0] = VISITED;
+                q.push(i);
+            }
+        }
+    }
+}
+void DFS(int startIndex){
+    adjArr[0][startIndex] = VISITED;
+    cout<<startIndex<<" ";
+    for(int i=1; i<=N; i++){
+        if(adjArr[startIndex][i]==CONNECTED && adjArr[0][i]!=VISITED){
+            DFS(i);
+        }
+    }
+}
+int main(){
+    getInput();
+    DFS(startIndex);
+    cout<<endl;
+    BFS(startIndex);
+
+    return 0;
+}
