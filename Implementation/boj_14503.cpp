b8cf23748b443a7779be14fbb48e67d2c3a32399
Sujin Lee <krispediadot@gmail.com>
2020-07-27 22:40:26 +0900

    Mod: boj_14503.cpp Desc: add more comments

--- Implementation/boj_14503.cpp
+++ Implementation/boj_14503.cpp
@@ -41,13 +41,13 @@ int main() {
             cin>>arr[i][j];
         }
     }
-
     if(arr[curX][curY]==NOTCLEAN)
         arr[curX][curY]=CLEAN;
         count++;
-
         while(1){
+            // 방향의 왼쪽 보기 
             int dir = (curDir+3)%4;
+            // a.왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한칸 이동  
             if(arr[curX+dirX[dir]][curY+dirY[dir]]==NOTCLEAN){
                 arr[curX+dirX[dir]][curY+dirY[dir]]=CLEAN;
                 count++;
@@ -55,13 +55,16 @@ int main() {
                 curX += dirX[dir];
                 curY += dirY[dir];
             }
+            // c. 4방향 모두 청소가 되어 있거나 벽인 경우, 바라보는 방향 유지 및 한칸 후진  
             else if(check4dirCleanWall(curX, curY)){
                 int backDir = (curDir+2)%4;
+                // d. 뒤쪽이 벽이라 후진할 수 없는 경우, 종료  
                 if(arr[curX+dirX[backDir]][curY+dirY[backDir]]==WALL)
                     break;
                 curX += dirX[backDir];
                 curY += dirY[backDir];
             }
+            // b. 왼쪽 방향에 청소할 공간이 없다면, 왼쪽 방향으로 회전  
             else{
                 curDir = dir;
             }
