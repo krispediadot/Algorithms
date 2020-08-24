40f876799c5a94ab12d71894f79227b82b03f9f9
Sujin Lee <krispediadot@gmail.com>
2020-08-24 11:54:38 +0900

    Add: boj_17863.cpp Desc: boj 17863.FYI solution

---
+++ String/boj_17863.cpp
@@ -0,0 +1,14 @@
+nclude<iostream>
+
+using namespace std;
+
+int main(){
+	    int num; cin>>num;
+		    num /= 10000;
+			    if(num==555)
+					        cout<<"YES"<<endl;
+				    else
+						        cout<<"NO"<<endl;
+
+					    return 0;
+}
