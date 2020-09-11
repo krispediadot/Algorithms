ad42ce90a84d3dbcce689f308654d36ecd7b005c
Sujin Lee <krispediadot@gmail.com>
2020-09-11 20:59:21 +0900

    Mod: 괄호변환.cpp Desc: 2020 kakao blind solution

--- Programmers/2020/괄호변환.cpp
+++ Programmers/2020/괄호변환.cpp
@@ -4,19 +4,15 @@
 
 using namespace std;
 
-vector<string> split(string s){
+int splitIndex(string s){
     int in=0; int index;
     for(int i=0; i<s.length(); i++){
         if(s[i]=='(') in++;
         if(in*2 == i+1) {
             index = i;
-            break;
+            return index;
         }
     }
-    vector<string> res;
-    res.push_back(s.substr(0,index+1));
-    res.push_back(s.substr(index+1));
-    return res;
 }
 bool isCorrect(string s){
     int in=0;
@@ -29,42 +25,24 @@ bool isCorrect(string s){
     }
     return true;
 }
-string convert(string target){
+string solution(string target){
     string answer = "";
 
     if(target.length()==0)
         return target;
 
     // u,v 나누기
-    vector<string> sp = split(target);
-    string u = sp[0];
-    string v = sp[1];
+    int index = splitIndex(target);
+    string u = target.substr(0, index+1);
+    string v = target.substr(index+1);
     // u 올바른 괄호인지 확인
     // 올바르다면
-    if(isCorrect(u)) {
-        // 1. 결과에 붙이고 v 손보기
-        answer += u;
-        answer += convert(v);
-    }
-        // 올바르지 않다면
-    else {
-        // 1. 결과에 ( 붙이고
-        answer += '(';
-        // 2. 옿바르게 정렬한 v 붙이고
-        answer += convert(v);
-        // 3. ) 붙이고
-        answer += ')';
-        // 4. u 앞 뒤 제거
-        // 5. u 괄호 뒤집어서 붙이기
-        for(int i=1; i<u.length()-1; i++){
-            if(u[i]==')') answer+='(';
-            else answer+=')';
-        }
-    }
-    return answer;
-}
-string solution(string p){
-    return convert(p);
+    // 1. 결과에 붙이고 v 손보기
+    if(isCorrect(u)) return u + solution(v);
+    // 올바르지 않다면
+    // 1. ( 붙이고 2. v 손보고 3. ) 붙이고 4.u 1~size-1까지 괄호 바꿔서 출력
+    for(char &c: u) c = c=='('?')':'(';
+    return '('+solution(v)+')'+u.substr(1,u.length()-2);
 }
 
 int main(){
