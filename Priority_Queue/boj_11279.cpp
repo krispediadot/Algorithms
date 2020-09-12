4806bdd730da04026590d6d2ec18a58888a77767
Sujin Lee <krispediadot@gmail.com>
2020-09-12 12:45:14 +0900

    Add: boj_11279.cpp Desc: boj 11279.최대힙 solution

---
+++ Priority_Queue/boj_11279.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    priority_queue<int> pq;
+    int n; cin>>n;
+
+    for(int i=0; i<n; i++){
+        int num; cin>>num;
+        if(num==0){
+            if(pq.empty())
+                cout<<0<<"\n";
+            else{
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
+        } else
+            pq.push(num);
+    }
+
+}
