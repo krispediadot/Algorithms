e1ba0889993e2734d4f0d759f75e1ca430e402cb
Sujin Lee <krispediadot@gmail.com>
2021-04-29 10:05:57 +0900

    Solved: leetcode 337. House Robber 3

---
+++ bfs_dfs/leetcode_337.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<TreeNode*, int> m;
+public:
+    int rob(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (m.find(root) != m.end()) return m[root];
+        
+        int total = 0;
+        if (root->left) total += (rob(root->left->left) + rob(root->left->right));
+        if (root->right) total += (rob(root->right->left) + rob(root->right->right));
+        return m[root] = max(total + root->val , (rob(root->left) + rob(root->right)));
+    }
+};
