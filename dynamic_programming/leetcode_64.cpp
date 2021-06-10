e9456cff20dfcfe58fdcd2a3f9f8e251051f6be3
Sujin Lee <krispediadot@gmail.com>
2021-06-10 10:33:26 +0900

    Solved: leetcode 64. Minimum Path Sum

--- dynamic_programming/leetcode_64.cpp
+++ dynamic_programming/leetcode_64.cpp
@@ -1,6 +1,5 @@
-
-// 12ms, 00:11:29
-class Solution {
+// 12ms, 10.1MB, 00:11:29
+class Solution1 {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int r_size = grid.size();
@@ -18,5 +17,25 @@ public:
         }
          
         return result.back().back();
+    }
+};
+
+// 8ms, 9.5MB, 00:11:29
+class Solution2 {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int r_size = grid.size();
+        int c_size = grid[0].size();
+
+        for (int r = 1; r < r_size; r++) grid[r][0] += grid[r - 1][0];
+        for (int c = 1; c < c_size; c++) grid[0][c] += grid[0][c - 1];
+
+        for (int r = 1; r < r_size; r++) {
+            for (int c = 1;   c < c_size; c++) {
+                grid[r][c] += min(grid[r - 1][c], grid[r][c - 1]);
+            }
+        }
+
+        return grid.back().back();
     }
 };
