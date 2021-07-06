f8539ea0bc08fe97b15a9f6822af029997dae689
Sujin Lee <krispediadot@gmail.com>
2021-07-06 15:01:26 +0900

    Solved: leetcode 112. Path Sum

--- bfs_dfs/leetcode_112.cpp
+++ bfs_dfs/leetcode_112.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// 12ms, 21.3MB
 class Solution {
 public:
     bool possible(TreeNode* root, int targetSum, int sum) {
@@ -22,5 +24,17 @@ public:
     }
     bool hasPathSum(TreeNode* root, int targetSum) {       
         return root == nullptr ? false : possible(root, targetSum, 0);
+    }
+};
+
+// 12ms, 21MB
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+         if (root->left == nullptr && root->right == nullptr)
+             return (targetSum - root->val == 0) ? true : false;
+
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
     }
 };
