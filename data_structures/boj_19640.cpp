29fb07be8d6043efb7b15a2437f84191885a77a1
Sujin Lee <krispediadot@gmail.com>
2021-10-06 15:52:11 +0900

    Solved: boj 19640. 화장실의 규칙

---
+++ data_structures/boj_19640.cpp
@@ -0,0 +1,79 @@
+// 10316 KB, 56ms
+
+// M 개의 줄을 vector로 구현 후 인덱스만 이동하기
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+ 
+struct cmp {
+
+    // D 높을수록, H 높을수록, line idx 낮을수록 우선
+
+    bool operator()(pair<pair<int, int>,int>& a, pair<pair<int, int>,int>& b) {
+        if (a.first.first == b.first.first) {
+            if (a.first.second == b.first.second) 
+                return a.second > b.second;
+            return a.first.second < b.first.second;
+        }
+        return a.first.first < b.first.first;
+    }
+};
+
+int main() {
+
+    // 1. input
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, M, K; cin >> N >> M >> K;
+
+    pair<int, int> DECA = {-1, -1}; // line idx, elem idx 
+
+    vector<vector<pair<int, int>>> lines(M);
+    vector<int> linesIdx(M, 0);
+
+    for (int i = 0; i < N; i++) {
+        int targetIdx = i % M;
+        pair<int, int> person; cin >> person.first >> person.second;
+        lines[targetIdx].push_back(person);
+
+        if (i == K) DECA = {targetIdx, (int)lines[targetIdx].size()-1};
+    }
+
+    // 2. logic
+
+    // 2-1. 각 줄의 첫번째 사람 정보 넣기
+    priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, cmp> pq; // info(D, H), lineIdx
+    for (int i = 0; i < M; i++) {
+        if (!lines[i].empty()) {
+            pq.push({lines[i].front(), i});
+            linesIdx[i]++;
+        }
+    }
+
+    int answer = 0;
+
+    // 2-2. 우선순위대로 빼기
+    while (!(pq.top().second == DECA.first && DECA.second == 0)) {
+        auto target = pq.top(); pq.pop();
+
+        // DECA인지 아닌지 확인하기 위해 DECA의 elem idx 빼기(elem idx==0이되면 DECA임)
+        if (target.second == DECA.first) DECA.second--;
+
+        answer++;
+        
+        // M 개의 각 줄에 사람이 있는지 없는지 확인 
+        if (linesIdx[target.second] < lines[target.second].size()) {
+            pq.push({lines[target.second][linesIdx[target.second]], target.second});
+            linesIdx[target.second]++;
+        }
+    } 
+
+    // 3. output
+
+    cout << answer;
+
+    return 0;
+}
