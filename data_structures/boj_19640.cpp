2e59a6499f0eb6bb90f90d1719f543ff26d27e9f
Sujin Lee <krispediadot@gmail.com>
2021-10-11 12:37:16 +0900

    Solved: boj 19640. 화장실의 규칙

--- data_structures/boj_19640.cpp
+++ data_structures/boj_19640.cpp
@@ -77,3 +77,70 @@ int main() {
 
     return 0;
 }
+
+// 10316 KB, 48ms
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct cmp {
+    bool operator()(pair<pair<int, int>, int> a, pair<pair<int, int>, int> b) {
+        if (a.first.first == b.first.first) {
+            if (a.first.second == b.first.second) {
+                return a.second > b.second;
+            }
+            return a.first.second < b.first.second;
+        }
+        return a.first.first < b.first.first;
+    }
+};
+
+int main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int N, M, K; cin >> N >> M >> K;
+
+    vector<vector<pair<int, int>>> lines(M);
+    vector<int> linesIdx(M, 0); // 선두 확인용
+
+    pair<int, int> DECA = {-1, -1}; // {targetLine, idx}
+
+    for (int i = 0; i < N; i++) {
+        int D, H; cin >> D >> H;
+
+        int targetLine = i % M;
+        lines[targetLine].push_back({D, H});
+
+        if (i == K) DECA = {targetLine, lines[targetLine].size()-1};
+    }
+
+    priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, cmp> pq;
+    
+    for (int i = 0; i < M; i++) {
+        if (!lines[i].empty()){
+            pq.push({lines[i][0], i});
+            linesIdx[i]++;
+        }
+    } 
+
+    int answer = 0;
+
+    while (!(pq.top().second == DECA.first && DECA.second == 0)) {
+        auto target = pq.top(); pq.pop();
+        int& line = target.second;
+
+        if (line == DECA.first) DECA.second--;
+        
+        answer++;
+
+        if (linesIdx[line] < lines[line].size())
+            pq.push({lines[line][linesIdx[line]++], line});
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
