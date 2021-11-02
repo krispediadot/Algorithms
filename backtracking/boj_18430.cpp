78e74fd7bf18eb1518add938928922af37e97591
Sujin Lee <krispediadot@gmail.com>
2021-11-02 09:38:52 +0900

    Solved: boj 18430. 무기 공학

--- backtracking/boj_18430.cpp
+++ backtracking/boj_18430.cpp
@@ -1,3 +1,78 @@
+// 2020 KB, 12ms
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int dirR[] = {-1, 0, 1, 0};
+const int dirC[] = {0, 1, 0, -1};
+
+int plate[5][5];
+bool used[5][5];
+int n, m;
+
+bool isValid(pair<int, int> pos, int& n, int& m) {
+    return pos.first >= 0 && pos.first < n && pos.second >= 0 && pos.second < m;
+}
+
+void makeCurve(pair<int, int> pos, int& answer, int sum) {
+
+    if (pos.second >= m) {
+        pos.second = 0;
+        pos.first++;
+        makeCurve(pos, answer, sum);
+        return;
+    }
+
+    if (pos.first >= n) {
+        if (sum > answer) answer = sum;
+        return;
+    }
+
+    if (used[pos.first][pos.second] == false) {
+        used[pos.first][pos.second] = true;
+
+        for (int dir = 0; dir < 4; dir++) {
+            pair<int, int> pos1 = {pos.first + dirR[dir], pos.second + dirC[dir]};
+            pair<int, int> pos2 = {pos.first + dirR[(dir+1)%4], pos.second + dirC[(dir+1)%4]};
+
+            if (isValid(pos1, n, m) && isValid(pos2, n, m) && used[pos1.first][pos1.second] == false && used[pos2.first][pos2.second] == false) {
+                
+                used[pos1.first][pos1.second] = true;
+                used[pos2.first][pos2.second] = true;
+                
+                int hardness = (plate[pos.first][pos.second]) * 2 + plate[pos1.first][pos1.second] + plate[pos2.first][pos2.second];
+
+                makeCurve({pos.first, pos.second+1}, answer, sum + hardness);
+
+                used[pos1.first][pos1.second] = false;
+                used[pos2.first][pos2.second] = false;
+            }
+        }
+
+        used[pos.first][pos.second] = false;
+    }
+
+    makeCurve({pos.first, pos.second+1}, answer, sum);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> plate[i][j];
+        }
+    }
+
+    int answer = 0;
+    makeCurve({0, 0}, answer, 0);
+
+    cout << answer;
+
+    return 0;
+}
+
 // 2156 KB, 280ms
 #include <iostream>
 #include <vector>
