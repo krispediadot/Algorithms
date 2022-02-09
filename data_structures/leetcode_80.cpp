8bfbe374aac1bc38fc94f9c21037b6b7517cdcd8
Sujin Lee <krispediadot@gmail.com>
2022-02-09 10:57:53 +0900

    Solved: leetcode 80. Remove Duplicates from Sorted Array2

--- data_structures/leetcode_80.cpp
+++ data_structures/leetcode_80.cpp
@@ -18,3 +18,22 @@ public:
         return nums.size() - sub;
     }
 };
+
+// 10.9MB, 10ms
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cnt = 2;
+        int sub = 0;
+        
+        if (nums.size() < 3)
+            return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[i-2-sub]) nums[cnt++] = nums[i];
+            else sub++;
+        }
+        
+        return cnt;
+    }
+};
