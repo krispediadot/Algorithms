b950b87adf157bba9f89a0bbac4ea6ae67f4623a
Sujin Lee <krispediadot@gmail.com>
2022-03-11 11:21:03 +0900

    Solved: leetcode 139. Word Break

--- dynamic_programming/leetcode_139.cpp
+++ dynamic_programming/leetcode_139.cpp
@@ -1,3 +1,28 @@
+// 9.2MB, 4ms
+class Solution {
+    unordered_map<string, bool> m; //{string, bool}
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        if (m.find(s) != m.end()) return m[s];
+        
+        bool answer = false;
+        
+        for (auto w : wordDict) {
+            int len = w.length();
+
+            if (w == s.substr(0, len)) {
+                if (len == s.length()) answer = true;
+                else answer |= wordBreak(s.substr(len), wordDict);
+            }
+            
+            if (answer) break;
+        }
+        
+        m.insert({s, answer});
+        return answer;
+    }
+};
+
 // 14.6MB, 34ms
 class Solution {
     unordered_map<string, bool> m;
