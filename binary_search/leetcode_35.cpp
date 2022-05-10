6516eaab51b4516fea91b55918ef84efd046d23e
Sujin Lee <krispediadot@gmail.com>
2022-05-10 11:09:35 +0900

    Solved: leetcode 35. Search Insert Portion

---
+++ binary_search/leetcode_35.cpp
@@ -0,0 +1,19 @@
+// 9.6MB, 6ms
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        
+        while (left <= right) {
+            
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        
+        return right + (right < 0 || nums[right] != target);
+    }
+};
